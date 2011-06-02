5a5bba3acb1a964fcfa83f9029cd0a6bc8127593
Alex Skorulis <skorulis@gmail.com>
2011-06-03 07:21:04 +1000

    Moved to post image loading as order of operations between platforms was causing issues

--- src/com/skorulis/heli3/components/AnimatedImage.java
+++ src/com/skorulis/heli3/components/AnimatedImage.java
@@ -2,6 +2,7 @@ package com.skorulis.heli3.components;
 
 import forplay.core.Image;
 import forplay.core.ImageLayer;
+import forplay.core.ResourceCallback;
 
 import static forplay.core.ForPlay.*;
 
@@ -13,6 +14,10 @@ public class AnimatedImage {
   private float currentTime;  
   private ImageLayer layer;
   
+  public AnimatedImage() {
+    layer = graphics().createImageLayer();
+  }
+  
   public AnimatedImage(Image[] frames) {
     this.frames = frames;
     this.index = 0; this.currentTime = 0;
@@ -21,10 +26,20 @@ public class AnimatedImage {
   
   public AnimatedImage(String[] imagePaths) {
     this.frames = new Image[imagePaths.length];
+    layer = graphics().createImageLayer();
     for(int i=0; i < imagePaths.length; ++i) {
       this.frames[i] = assetManager().getImage(imagePaths[i]);
     }
-    layer = graphics().createImageLayer(frames[index]);
+    
+  }
+  
+  public void setImages(String[] paths,ResourceCallback<Image> callback) {
+    this.frames = new Image[paths.length];
+    for(int i=0; i < paths.length; ++i) {
+      this.frames[i] = assetManager().getImage(paths[i]);
+      this.frames[i].addCallback(callback); 
+    }
+    layer.setImage(frames[index]);
   }
   
   public void update(float delta) {
