2f6bf0cc9517fa52edcfeecd0b53a24310a99aae
Alex Skorulis <skorulis@gmail.com>
2011-06-07 18:37:27 +1000

    Moved code to forplay-support

--- src/com/skorulis/heli3/core/HeliWorld.java
+++ src/com/skorulis/heli3/core/HeliWorld.java
@@ -1,60 +1,36 @@
 package com.skorulis.heli3.core;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.Iterator;
 
-import org.jbox2d.callbacks.DebugDraw;
-import org.jbox2d.collision.AABB;
-import org.jbox2d.collision.shapes.PolygonShape;
 import org.jbox2d.common.Vec2;
-import org.jbox2d.dynamics.Body;
-import org.jbox2d.dynamics.BodyDef;
 import org.jbox2d.dynamics.World;
 
 import com.skorulis.forplay.entities.Entity;
 import com.skorulis.forplay.entities.Event;
+import com.skorulis.forplay.util.EntityWorld;
 import com.skorulis.forplay.util.InputState;
 import com.skorulis.forplay.util.WorldUtil;
+import com.skorulis.heli3.components.HeliEntity;
 
 import static forplay.core.ForPlay.*;
 
-import forplay.core.CanvasLayer;
 import forplay.core.DebugDrawBox2D;
 import forplay.core.GroupLayer;
 
-public class HeliWorld {
+public class HeliWorld extends EntityWorld<HeliEntity> {
 
-	private World world;
 	private Heli3Game game;
-	private LinkedList<Entity> entities,newEntities;
-	private GroupLayer entityLayer;
-	
-	private static boolean showDebugDraw = true;
-  private DebugDrawBox2D debugDraw;
-  private float width,height;
 	
 	public HeliWorld(Heli3Game game,float width,float height,float physScale) {
-	  this.game = game;
-	  this.width = width*physScale;
-	  this.height = height*physScale;
-	  Vec2 gravity = new Vec2(0,80*physScale);
-	  world = new World(gravity, true);
-	  entities = new LinkedList<Entity>();
-	  newEntities = new LinkedList<Entity>();
-	  entityLayer = graphics().createGroupLayer();
-	  graphics().rootLayer().add(entityLayer);
-	  WorldUtil.buildBounds(this.width, this.height,world);
-	  entityLayer.setScale(1f / Heli3Game.physUnitPerScreenUnit);
-	  
-	  if(showDebugDraw) {
-	    debugDraw = WorldUtil.showDebugDraw(this.width, this.height, physScale, world);
-	  } 
+	  super(width,height,new Vec2(0,80*physScale),physScale,true);
+	  this.game = game;	 
 	}
 	
 	public void update(float delta,InputState input) {
 	  world.step(delta, 10, 10);
-	  java.util.Iterator<Entity> it = entities.iterator();
-    Entity e;
+	  Iterator<HeliEntity> it = entities.iterator();
+    HeliEntity e;
     ArrayList<Event> events;
     while(it.hasNext()) {
       e = it.next();
@@ -75,32 +51,16 @@ public class HeliWorld {
     
 	}
 	
-	public void paint(float alpha) {
-	  java.util.Iterator<Entity> it = entities.iterator();
-	  Entity e;
+	public HeliEntity findEntity(Class type,int team) {
+	  Iterator<HeliEntity> it = entities.iterator();
+	  HeliEntity e;
 	  while(it.hasNext()) {
 	    e = it.next();
-	    e.paint(alpha);
+	    if(e.getClass()==type && e.team()==team) {
+	      return e;
+	    }
 	  }
-	  
-	  if (showDebugDraw) {
-      debugDraw.getCanvas().canvas().clear();
-      world.drawDebugData();
-    }
-	  
-	}
-	
-	public World world() {
-	  return world;
-	}
-	
-	public void addEntity(Entity e) {
-	  newEntities.add(e);
-	  entityLayer.add(e.layer());
-	}
-	
-	public GroupLayer entityLayer() {
-	  return entityLayer;
+	  return null;
 	}
 	
 }
